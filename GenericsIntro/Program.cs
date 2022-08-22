b3e6863df8d21dd0bc30deaafca88aacf51a952c
DreamerGenco <sevdingencer@gmail.com>
2022-08-22 22:47:14 +0300

    22 Ağustos Ders Sonu

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GenericsIntro;
+
+
+MyList <string>isimler = new MyList<string>();
+isimler.Add("Gencer");
+
+
+
+
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+//string[] isimler = new string[] {"Engin","Murat","Gencer"};
+//Console.WriteLine(isimler[0]);
+//Console.WriteLine(isimler[1]);
+//Console.WriteLine(isimler[2]);
+
+//isimler = new string[4];
+//isimler[3] = "adasd";
+//Console.WriteLine(isimler[3]);
+//Console.WriteLine(isimler[0]);
+
+List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+Console.WriteLine(isimler2[0]);
+
+Console.WriteLine(isimler2[1]);
+Console.WriteLine(isimler2[2]);
+
+Console.WriteLine(isimler2[3]);
+isimler2.Add("İlker");
+Console.WriteLine(isimler2[4]);
+Console.WriteLine(isimler2[0]);
