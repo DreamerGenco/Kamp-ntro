040b32dc19c48582a7e6af6d673cabadaf93715d
Gencer <sevdingencer@gmail.com>
2023-07-27 18:01:16 +0300

    .27/07/2023 OOP3

--- OOP2/Program.cs
+++ OOP2/Program.cs
@@ -5,14 +5,14 @@
         static void Main(string[] args)
         {
             GercekMusteri musteri1 = new GercekMusteri();
-            musteri1.Id = 1
+            musteri1.Id = 1;
             musteri1.MusteriNo = "12345";
             musteri1.Adi = "Engin";
             musteri1.Soyadi = "DEMİROĞ";
             musteri1.TcNo = "1223425123";
 
-            TuzelMusteri musteri2 = new TuzelMusteri()
-            musteri2.Id = 2
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
             musteri2.MusteriNo = "2345";
             musteri2.SirketAdi = "Kodlama.io";
             musteri2.VergiNo = "4524233";
---
+++ OOP3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            
+
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+           
+
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            BasvuruManager basvuruManager = new BasvuruManager();
+
+            basvuruManager.BasvuruYap(ihtiyacKrediManager);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { };
+            basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+            //Interfaceler de o ınterface'i implamente eden sınıfın referans numarasını tutabilir
+
+        }
+    }
+}
