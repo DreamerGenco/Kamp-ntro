4d9d74b9432094b8c9bc7afdae360738e2b4283c
Gencer <sevdingencer@gmail.com>
2023-08-02 18:11:11 +0300

    RecapDemo_gun_5_Odev_1 islendi.

--- RecapDemo_Gun_5_Odev_1/Form1.cs
+++ RecapDemo_Gun_5_Odev_1/Form1.cs
@@ -6,5 +6,25 @@ namespace RecapDemo_Gun_5_Odev_1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button[,] button = new Button[8,8];
+            for (int i = 0; i < button.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < button.GetUpperBound(1); j++)
+                {
+                    button[i,j] = new Button();
+                    button[i, j].Width = 50;
+                    button[i, j].Height = 50;
+                    this.Controls.Add(button[i, j]);
+
+                }
+            }
+             
+
+                
+
+        }
     }
 }
