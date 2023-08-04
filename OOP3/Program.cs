60ff93a28a3e6fe2e84275e5bc920c491ed839eb
DreamerGenco <sevdingencer@gmail.com>
2023-08-04 22:51:10 +0300

    .

--- OOP3/Program.cs
+++ OOP3/Program.cs
@@ -10,8 +10,7 @@ namespace OOP3
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager ihtiyacKrediManager = new IhtiyackrediManager();
             IKrediManager tasitKrediManager = new TasitKrediManager();
             IKrediManager konutKrediManager = new KonutKrediManager();
             
@@ -22,21 +21,16 @@ namespace OOP3
             basvuruManager.KrediOnbilgilendirmesiYap(krediler);
 
 
-=======
             IKrediManager konutKredi = new KonutKrediManager();
             IKrediManager tasitKredi = new TasitKrediManager();
             IKrediManager ihtiyacKredi = new IhtiyackrediManager();
             IKrediManager esnafKredi = new EsnafKrediManager();
             ILoggerService databaseLoggerService = new DataBaseLoggerService();
             ILoggerService fileLoggerService = new FileLoggerService();
-            BasvuruManager basvuruManager = new BasvuruManager();
             //basvuruManager.BasvuruYap(konutKredi, new DataBaseLoggerService());   
             List<ILoggerService>loggers = new List<ILoggerService> { new SmsLoggerService(), new DataBaseLoggerService()};   
-
-            List<IKrediManager> krediler = new List<IKrediManager>() {tasitKredi, esnafKredi};
             basvuruManager.BasvuruYap(esnafKredi, loggers);
             // basvuruManager.KrediOnBilgilendirmesiYap(krediler);
->>>>>>> 4d9d74b9432094b8c9bc7afdae360738e2b4283c
             Console.ReadKey();
         }
     }
