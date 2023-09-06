d91ddf42abe10c399df13304543567a4a938b502
Gencer <sevdingencer@gmail.com>
2023-09-06 23:14:42 +0300

    OOP3 Bank Credit Calculator

---
+++ OOP3 _BankCreditCalculator/MortgageLoanManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3__BankCreditCalculator
+{
+    internal class MortgageLoanManager:ICreditManager
+    {
+        public void Calculate()
+        {
+            Console.WriteLine("Mortgage loan has been calculated");
+        }
+
+    }
+}
