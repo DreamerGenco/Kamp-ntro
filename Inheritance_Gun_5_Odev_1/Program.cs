721d013bc07ac8d2ecd5371e19df57aabbb05147
Gencer <sevdingencer@gmail.com>
2023-09-08 10:01:21 +0300

    abc

---
+++ Inheritance_Gun_5_Odev_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Gun_5_Odev_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer(2);
+            Person[] persons = new Person[] { new Customer(), new student() };
+        }
+    }
+}
---
+++ Inheritance_Gun_5_Odev_1/student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Gun_5_Odev_1
+{
+    internal class student:Person
+    {
+        public string Department { get; set; }
+
+    }
+}
